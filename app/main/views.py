71c04d9f1109a1cdb8baa13eba0e8dac0d94a8de
RobbieClarken <robbie.clarken@gmail.com>
2016-01-12 17:56:20 +1100

    Add shipment page

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
 from . import main
 from .. import mongo
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, abort
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class ShipmentForm(Form):
-    name = StringField('Full Name', validators=[DataRequired()])
+    owner = StringField('Full Name', validators=[DataRequired()])
+    epn = StringField('EPN', validators=[DataRequired()])
     submit = SubmitField()
 
 
@@ -16,9 +17,24 @@ class ShipmentForm(Form):
 def index():
     form = ShipmentForm()
     if form.validate_on_submit():
+        epn = form.data['epn']
+        shipment_id = mongo.db.dewars.find().count() + 1
+        epn_dewar_num = mongo.db.dewars.find({'epn': epn}).count() + 1
+        dewar_name = 'd-{epn}-{n}'.format(epn=epn, n=epn_dewar_num)
         dewar = {
-            'owner': form.data['name'],
+            'shipment_id': shipment_id,
+            'name': dewar_name,
+            'owner': form.data['owner'],
+            'epn': epn,
         }
         mongo.db.dewars.insert(dewar)
-        return redirect(url_for('.index'))
+        return redirect(url_for('.shipment', shipment_id=shipment_id))
     return render_template('index.html', form=form)
+
+
+@main.route('/shipment/<int:shipment_id>')
+def shipment(shipment_id):
+    dewars = list(mongo.db.dewars.find({'shipment_id': shipment_id}))
+    if not dewars:
+        abort(404)
+    return render_template('shipment.html', dewars=dewars)
--- tests/views/test_views.py
+++ tests/views/test_views.py
@@ -31,11 +31,28 @@ def test_index_renders(app):
 def test_form_submits(app):
     client = app.test_client()
     data = {
-        'name': 'Jane',
+        'owner': 'Jane',
+        'epn': '123',
     }
     response = client.post(url_for('main.index'), data=data)
     assert response.status_code == 302
     dewars = list(mongo.db.dewars.find())
     assert len(dewars) == 1
     dewar = dewars[0]
+    assert dewar['shipment_id'] == 1
+    assert dewar['name'] == 'd-123-1'
     assert dewar['owner'] == 'Jane'
+    assert dewar['epn'] == '123'
+
+
+def test_shipment_view(app):
+    client = app.test_client()
+    dewar = {
+        'shipment_id': 1,
+        'name': 'd-123-1',
+        'owner': 'Jane',
+    }
+    mongo.db.dewars.insert(dewar)
+    response = client.get(url_for('main.shipment', shipment_id=1))
+    html = response.data.decode('utf-8')
+    assert 'The Dewar ID is: d-123-1' in html
