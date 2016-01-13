15450dfdccb200a0d05740bf2e9bf671c0e91eba
RobbieClarken <robbie.clarken@gmail.com>
2016-01-13 13:13:50 +1100

    Store return address and container info to the database
    Show return address on shipment page

--- app/main/views.py
+++ app/main/views.py
@@ -3,13 +3,33 @@ from .. import mongo
 from ..utils import arrival_url
 from flask import render_template, url_for, redirect, abort
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired
+import uuid
 
 
 class ShipmentForm(Form):
     owner = StringField('Full Name', validators=[DataRequired()])
+    department = StringField('Department')
+    institute = StringField('Institute')
+    street_address = StringField('Street Address')
+    city = StringField('City')
+    state = StringField('State')
+    postcode = StringField('Post Code')
+    country = StringField('Country')
+    phone = StringField('Contact Phone Number')
+    email = StringField('Contact Email')
     epn = StringField('EPN', validators=[DataRequired()])
+    return_dewar = BooleanField('Return dewar?')
+    courier = StringField('Courier')
+    courier_account = StringField('Courier Account Number')
+    container_type = SelectField('Sample Type',
+                              choices=[('cassette', 'cassette'),
+                                       ('pucks', 'pucks'),
+                                       ('canes', 'canes'),
+                                      ])
+    container_ids_1 = StringField('ID(s) for adaptor/cassette 1')
+    container_ids_2 = StringField('ID(s) for adaptor/cassette 2')
     submit = SubmitField()
 
 
@@ -18,21 +38,36 @@ def index():
     form = ShipmentForm()
     if form.validate_on_submit():
         epn = form.data['epn']
-        shipment_id = mongo.db.dewars.find().count() + 1
+        shipment_id = str(uuid.uuid4())
         epn_dewar_num = mongo.db.dewars.find({'epn': epn}).count() + 1
         dewar_name = 'd-{epn}-{n}'.format(epn=epn, n=epn_dewar_num)
+        containers = '{container_ids_1} | {container_ids_2}'.format(**form.data)
         dewar = {
             'shipment_id': shipment_id,
             'name': dewar_name,
-            'owner': form.data['owner'],
             'epn': epn,
+            'owner': form.data['owner'],
+            'department': form.data['department'],
+            'institute': form.data['institute'],
+            'streetAddress': form.data['street_address'],
+            'city': form.data['city'],
+            'state': form.data['state'],
+            'postcode': form.data['postcode'],
+            'country': form.data['country'],
+            'phone': form.data['phone'],
+            'email': form.data['email'],
+            'returnDewar': form.data['return_dewar'],
+            'courier': form.data['courier'],
+            'courierAccount': form.data['courier_account'],
+            'containerType': form.data['container_type'],
+            'expectedContainers': containers,
         }
         mongo.db.dewars.insert(dewar)
         return redirect(url_for('.shipment', shipment_id=shipment_id))
     return render_template('index.html', form=form)
 
 
-@main.route('/shipment/<int:shipment_id>')
+@main.route('/shipment/<shipment_id>')
 def shipment(shipment_id):
     dewars = list(mongo.db.dewars.find({'shipment_id': shipment_id}))
     for dewar in dewars:
--- tests/views/test_views.py
+++ tests/views/test_views.py
@@ -32,27 +32,72 @@ def test_form_submits(app):
     client = app.test_client()
     data = {
         'owner': 'Jane',
+        'department': 'Chemistry',
+        'institute': 'Some University',
+        'street_address': '123 Main Road',
+        'city': 'Brisbane',
+        'state': 'Queensland',
+        'postcode': '3000',
+        'country': 'Australia',
+        'phone': '111-222-333',
+        'email': 'jane@example.com',
         'epn': '123',
+        'return_dewar': 'y',
+        'courier': 'Fast Deliveries',
+        'courier_account': '999',
+        'container_type': 'pucks',
+        'container_ids_1': 'ASP001,ASP002',
+        'container_ids_2': 'ASP003',
     }
     response = client.post(url_for('main.index'), data=data)
     assert response.status_code == 302
     dewars = list(mongo.db.dewars.find())
     assert len(dewars) == 1
     dewar = dewars[0]
-    assert dewar['shipment_id'] == 1
+    assert isinstance(dewar['shipment_id'], str)
     assert dewar['name'] == 'd-123-1'
-    assert dewar['owner'] == 'Jane'
     assert dewar['epn'] == '123'
+    assert dewar['owner'] == 'Jane'
+    assert dewar['department'] == 'Chemistry'
+    assert dewar['institute'] == 'Some University'
+    assert dewar['streetAddress'] == '123 Main Road'
+    assert dewar['city'] == 'Brisbane'
+    assert dewar['state'] == 'Queensland'
+    assert dewar['postcode'] == '3000'
+    assert dewar['country'] == 'Australia'
+    assert dewar['phone'] == '111-222-333'
+    assert dewar['email'] == 'jane@example.com'
+    assert dewar['returnDewar'] == True
+    assert dewar['courier'] == 'Fast Deliveries'
+    assert dewar['courierAccount'] == '999'
+    assert dewar['containerType'] == 'pucks'
+    assert dewar['expectedContainers'] == 'ASP001,ASP002 | ASP003'
 
 
 def test_shipment_view(app):
     client = app.test_client()
     dewar = {
-        'shipment_id': 1,
+        'shipment_id': '1a',
         'name': 'd-123-1',
+        'epn': '123',
         'owner': 'Jane',
+        'department': 'Chemistry',
+        'institute': 'Some University',
+        'streetAddress': '123 Main Road',
+        'city': 'Brisbane',
+        'state': 'Queensland',
+        'postcode': '3000',
+        'country': 'Australia',
+        'phone': '111-222-333',
+        'email': 'jane@example.com',
+        'returnDewar': True,
+        'courier': 'Fast Deliveries',
+        'courierAccount': '999',
+        'containerType': 'pucks',
+        'expectedContainers': 'ASP001,ASP002 | ASP003',
     }
     mongo.db.dewars.insert(dewar)
-    response = client.get(url_for('main.shipment', shipment_id=1))
+    response = client.get(url_for('main.shipment', shipment_id='1a'))
     html = response.data.decode('utf-8')
     assert 'The Dewar ID is: d-123-1' in html
+    assert '123 Main Road' in html
